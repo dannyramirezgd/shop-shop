45ef8b7cc813ffe523113596d0c733c42a962eee
Danny Ramirez <danny.ramirezgd@gmail.com>
2022-04-18 14:19:01 -0400

    global start established

--- client/src/utils/GlobalState.js
+++ client/src/utils/GlobalState.js
@@ -3,3 +3,20 @@ import { useProductReducer } from './reducers';
 
 const StoreContext = createContext();
 const { Provider } = StoreContext;
+
+const StoreProvider = ({ value = [], ...props}) => {
+    const [state, dispatch] = useProductReducer({
+        products: [],
+        categories: [],
+        currentCategory: '',
+    });
+    console.log(state);
+    return <Provider value={[state, dispatch]} {...props} />;
+};
+
+const useStoreContext = () => {
+    return useContext(StoreContext);
+}
+
+export { StoreProvider, useStoreContext }; 
+
